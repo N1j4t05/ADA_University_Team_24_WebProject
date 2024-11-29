7864458454dc4095b9cb54c028953a938bcb3bf7
N1j4t05 <nicat08122005@gmail.com>
2024-11-29 22:53:19 +0400

    Fourth Commit

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ function moveForward(distance, context) {
     let a = radian(angle);
     x = x + distance * Math.cos(a);
     y = y + distance * Math.sin(a);
-    context.lineTo(x, y);    
+    context.lineTo(x, y);
 }
 
 function turnRight(degree) {
@@ -31,19 +31,16 @@ function turnLeft(degree) {
 }
 
 function DrawSpiral(context) {
-    // Inspired by Express Course (2024) Lesson 29: For Loops with Artist
-    // https://studio.code.org/s/express-2024/lessons/29/levels/5
 
-    // The initial position is in the center of the canvas
     x = context.canvas.width / 2;
     y = context.canvas.height / 2;
-    // The initial orientation is zero degrees i.e. facing East
-    angle = 0.0; 
+    angle = 0.0;
     context.moveTo(x, y);
     context.beginPath();
-    for (let counter = 3; counter < 600; counter += 3) {
+    for (let counter = 3; counter < 500; counter += 1) {
         moveForward(counter, context);
         context.stroke();
-        turnRight(89);
+        turnRight(299);
     }
+    context.closePath()
 }
